6bdf9c648947fea91d942e3f8831999a5e422d94
Duseldof777 <va.madunov@gmail.com>
2023-09-03 18:25:09 +0300

    add

--- src/cat-api.js
+++ src/cat-api.js
@@ -3,9 +3,11 @@ import axios from 'axios';
 axios.defaults.headers.common['x-api-key'] =
   'live_uDbSslA6LeI3y7iXTQiev0SGtT4FNOpDRiWdb5Ee5Q5TidAzDBnrm6TVRjy3mtrC';
 
+const BASE_URL = 'https://api.thecatapi.com/v1';
+
 function fetchBreeds() {
   return axios
-    .get('https://api.thecatapi.com/v1/breeds')
+    .get(`${BASE_URL}/breeds`)
     .then(res => {
       return res.data;
     })
@@ -16,7 +18,7 @@ function fetchBreeds() {
 
 function fetchCatByBreed(breedId) {
   return axios
-    .get(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`)
+    .get(`${BASE_URL}/images/search?breed_ids=${breedId}`)
     .then(res => {
       return res.data;
     })
